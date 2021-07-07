59d3edcde04c0a0baf1f401412929e5f2d8eadd1
CyronF <cyronpfraser@gmail.com>
2021-07-07 15:19:05 -0400

    edited http

--- seeds/product-seeds.js
+++ seeds/product-seeds.js
@@ -6,14 +6,14 @@ const productData = [
     price: 14.99,
     quantity: 14,
     category_id: 1,
-    image_url:"http://assets.stickpng.com/images/580b57fcd9996e24bc43c137.png"
+    image_url:"https://assets.stickpng.com/images/580b57fcd9996e24bc43c137.png"
   },  
   {
     name: 'apples',
     price: 14.99,
     quantity: 14,
     category_id: 1,
-    image_url:"http://assets.stickpng.com/images/580b57fcd9996e24bc43c137.png"
+    image_url:"https://assets.stickpng.com/images/580b57fcd9996e24bc43c137.png"
   },
   {
     name: 'Fabuloso',
