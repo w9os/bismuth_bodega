a61c77baaa417b4dc32a39f6c762fb85cbc26e84
CyronF <cyronpfraser@gmail.com>
2021-07-09 16:08:59 -0400

    created transaction handlebars, edited routes and home handlebars

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
 const categoryRoutes = require('./category-routes');
 const productRoutes = require('./product-routes');
-// const transactionRoutes = require('./transaction-routes');
+const transactionRoutes = require('./transaction-routes');
 const userRoutes = require('./user-routes');
 
 router.use('/categories', categoryRoutes);
 router.use('/products', productRoutes);
-// router.use('/transaction', transactionRoutes);
+router.use('/transaction', transactionRoutes);
 router.use('/user', userRoutes);
 
 module.exports = router;
--- routes/html/products.js
+++
@@ -1,6 +0,0 @@
-module.exports = (req, res) => {
-    res.render('products')}
-
-
-
-
